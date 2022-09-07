b648192b3665f225f558a8d40fad47d3a41a77c8
carlos-pv <paraqvaria.it.1@gmail.com>
2022-09-07 17:00:38 -0400

    add pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ItemComponent } from './pages/item/item.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'item', component: ItemComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'home'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
